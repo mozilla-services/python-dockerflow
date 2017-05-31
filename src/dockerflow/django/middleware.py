82ffaec93bb81ed56cc7a8a7c8281615c85e1caa
Peter Bengtsson <peterbe@mozilla.com>
2017-05-31 10:18:23 -0400

    Use request.user.is_authenticated as a property, fixes #14 (#15)

--- src/dockerflow/django/middleware.py
+++ src/dockerflow/django/middleware.py
@@ -3,12 +3,25 @@ import re
 import time
 import uuid
 
+from django import VERSION
 try:
     from django.utils.deprecation import MiddlewareMixin
 except ImportError:  # pragma: no cover
     MiddlewareMixin = object
 
 from . import views
+
+# Computed once, reused in every request
+_less_than_django_1_10 = VERSION < (1, 10)
+
+
+def is_authenticated(user):
+    """Check if the user is authenticated but do it in a way that
+    it doesnt' cause a DeprecationWarning in Django >=1.10"""
+    if _less_than_django_1_10:
+        # Prior to Django 1.10, user.is_authenticated was a method
+        return user.is_authenticated()
+    return user.is_authenticated
 
 
 class DockerflowMiddleware(MiddlewareMixin):
@@ -49,8 +62,10 @@ class DockerflowMiddleware(MiddlewareMixin):
         # modified earlier, so be sure to check for existence of these
         # attributes before trying to use them.
         if hasattr(request, 'user'):
-            out['uid'] = (request.user.is_authenticated() and
-                          request.user.pk or '')
+            out['uid'] = (
+                is_authenticated(request.user) and
+                request.user.pk or ''
+            )
         if hasattr(request, '_id'):
             out['rid'] = request._id
         if hasattr(request, '_logging_start_timestamp'):
