5f8c7cd24f61fc20285c0d8255784451c0acb817
Jannis Leidel <jannis@leidel.info>
2021-01-04 18:17:14 +0100

    Loopback again?

--- tests/django/settings.py
+++ tests/django/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = (
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/0",
+        "LOCATION": "redis://127.0.0.1:6379/0",
         "OPTIONS": {"REDIS_CLIENT_CLASS": "fakeredis.FakeStrictRedis"},
     }
 }
