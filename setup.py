f1651ee67bcbf940258fa778fac6c815b3aba382
Graham Beckley <gbeckley@mozilla.com>
2023-07-31 15:50:16 -0400

    Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -48,5 +47,5 @@ setup(
         "sanic": ["sanic"],
     },
     zip_safe=False,
-    python_requires=">=3.7,<4",
+    python_requires=">=3.8,<4",
 )
