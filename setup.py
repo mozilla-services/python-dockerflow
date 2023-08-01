5cc6b9f6be2249bf1dff02972c6567d945343897
Graham Beckley <gbeckley@mozilla.com>
2023-08-01 10:50:45 -0400

    Revert "Drop support for Python 3.7"
    This reverts commit f1651ee67bcbf940258fa778fac6c815b3aba382.

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -48,5 +49,5 @@ setup(
         "sanic": ["sanic"],
     },
     zip_safe=False,
-    python_requires=">=3.8,<4",
+    python_requires=">=3.7,<4",
 )
