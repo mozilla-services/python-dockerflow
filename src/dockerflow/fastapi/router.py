81417055635d60de7dd9e4d42ae674d5ecca08f6
Graham Beckley <gbeckley@mozilla.com>
2023-08-02 13:44:18 -0400

    Add version endpoint and tests

--- src/dockerflow/fastapi/router.py
+++ src/dockerflow/fastapi/router.py
@@ -1,7 +1,10 @@
+import os
+
 from fastapi import Request, Response
 from fastapi.routing import APIRouter
 
 
+from ..version import get_version
 dockerflow_router = APIRouter(tags=["Dockerflow"])
 
 
@@ -9,3 +12,14 @@ dockerflow_router = APIRouter(tags=["Dockerflow"])
 @dockerflow_router.head("/__lbheartbeat__")
 def lbheartbeat():
     return {"status": "ok"}
+
+
+@dockerflow_router.get("/__version__")
+def version(request: Request):
+    if getattr(request.app.state, "APP_DIR", None):
+        root = request.app.state.APP_DIR
+    elif os.getenv("APP_DIR"):
+        root = os.getenv("APP_DIR")
+    else:
+        root = "/app"
+    return get_version(root)
--- tests/fastapi/test_fastapi.py
+++ tests/fastapi/test_fastapi.py
@@ -1,6 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
+import json
 import logging
 
 import pytest
@@ -55,3 +56,39 @@ def test_mozlog(client, caplog):
     assert record.method == "GET"
     assert record.path == "/__lbheartbeat__"
     assert isinstance(record.t, int)
+
+
+VERSION_CONTENT = {"foo": "bar"}
+
+
+def test_version_app_state(client, tmp_path, app):
+    version_path = tmp_path / "version.json"
+    version_path.write_text(json.dumps(VERSION_CONTENT))
+
+    app.state.APP_DIR = tmp_path.resolve()
+    response = client.get("/__version__")
+    assert response.status_code == 200
+    assert response.json() == VERSION_CONTENT
+
+
+def test_version_env_var(client, tmp_path, monkeypatch):
+    version_path = tmp_path / "version.json"
+    version_path.write_text(json.dumps(VERSION_CONTENT))
+
+    monkeypatch.setenv("APP_DIR", tmp_path.resolve())
+
+    response = client.get("/__version__")
+    assert response.status_code == 200
+    assert response.json() == VERSION_CONTENT
+
+
+def test_version_default(client, mocker):
+    mock_get_version = mocker.MagicMock(return_value=VERSION_CONTENT)
+    mocker.patch("dockerflow.fastapi.router.get_version", mock_get_version)
+
+    response = client.get("/__version__")
+    assert response.status_code == 200
+    assert response.json() == VERSION_CONTENT
+    mock_get_version.assert_called_with("/app")
+
+
