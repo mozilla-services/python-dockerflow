033310fd08492e786a4480150a4d5edc8c3aeb20
Mathieu Leplatre <mathieu@mozilla.com>
2024-02-14 17:12:06 +0100

    Run latest black

--- tests/core/test_logging.py
+++ tests/core/test_logging.py
@@ -32,7 +32,8 @@ def assert_records(records):
 
 
 def test_initialization_from_ini(reset_logging, caplog, tmpdir):
-    ini_content = textwrap.dedent("""
+    ini_content = textwrap.dedent(
+        """
     [loggers]
     keys = root
 
@@ -54,7 +55,8 @@ def test_initialization_from_ini(reset_logging, caplog, tmpdir):
 
     [formatter_json]
     class = dockerflow.logging.JsonLogFormatter
-    """)
+    """
+    )
     ini_file = tmpdir.join("logging.ini")
     ini_file.write(ini_content)
     logging.config.fileConfig(str(ini_file))
@@ -154,7 +156,8 @@ def test_ignore_json_message(caplog):
 
 
 # https://mana.mozilla.org/wiki/pages/viewpage.action?pageId=42895640
-JSON_LOGGING_SCHEMA = json.loads("""
+JSON_LOGGING_SCHEMA = json.loads(
+    """
 {
     "type":"object",
     "required":["Timestamp"],
@@ -226,4 +229,7 @@ JSON_LOGGING_SCHEMA = json.loads("""
         }
     }
 }
-""".replace("\\", "\\\\"))  # HACK: Fix escaping for easy copy/paste
+""".replace(
+        "\\", "\\\\"
+    )
+)  # HACK: Fix escaping for easy copy/paste
